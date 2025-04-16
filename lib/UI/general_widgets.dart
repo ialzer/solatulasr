80edaecd26a29d00b928e826443333ce59bf90c7
ialzer <ialzeref@gmail.com>
2025-04-16 18:25:25 +0500

    Update general_widgets.dart
    aaaa

--- lib/UI/general_widgets.dart
+++ lib/UI/general_widgets.dart
@@ -1,34 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:solatulasr/UI/HomePage/home_page_widget.dart';
+
+class CustomBottomNavBarState extends GetX {
+  static RxInt selectedIndex = 0.obs;
+  CustomBottomNavBarState({super.key, required super.builder}) {
+    selectedIndex = 0.obs;
+  }
+  static void changeIndex(int index) {
+    selectedIndex = index.obs;
+  }
+}
 
 class GeneralWidgets extends StatelessWidget {
   const GeneralWidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<IconData> icons = [
+      Icons.home,
+      Icons.search,
+      Icons.notifications,
+      Icons.person,
+      Icons.settings,
+    ];
+
+    List<String> titles = [
+      'Home',
+      'Search',
+      'Notifications',
+      'Profile',
+      'Settings',
+    ];
     return Scaffold(
       body: HomePageWidget(),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blue, // Replace with a solid color
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        onTap: (index) {
-          // Handle navigation logic here
-        },
+      bottomNavigationBar: CustomBottomNavBarState(
+        builder:
+            (controller) => CustomBottomNavBar.items(
+              context,
+              icons,
+              titles,
+              CustomBottomNavBarState.selectedIndex.value,
+            ),
+      ),
+    );
+  }
+}
+
+class CustomBottomNavBar {
+  static Widget items(
+    context,
+    List<IconData> icons,
+    List<String> titles,
+    selectedIndex,
+  ) {
+    return Card(
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.lightBlueAccent,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha((0.1 * 255).toInt()),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        height: 100,
+        child: Row(
+          children: List.generate(icons.length - 1, (index) {
+            return InkWell(
+              onTap: () => CustomBottomNavBarState.changeIndex(index),
+              child: Expanded(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Icon(icons[index], color: Colors.white),
+                    Text(titles[index], style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
