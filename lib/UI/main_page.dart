6847c904429d13917c56fb773f5a84e5dc8a66ac
ialzer <ialzeref@gmail.com>
2025-05-26 19:48:04 +0500

    BNBar

--- lib/UI/main_page.dart
+++ lib/UI/main_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/state_manager.dart';
 import 'package:solatulasr/UI/bottom_navigate_bar_custom.dart';
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class MainPageController extends GetxController {
+  var currentIndex = 0.obs;
 
+  void changeIndex(int index) {
+    currentIndex.value = index;
+  }
+}
+
+class MainPage extends StatelessWidget {
+  MainPage({super.key});
+  final MainPageController controller = Get.put(MainPageController());
   @override
   Widget build(BuildContext context) {
     List<NavigationBarItem> items = [
@@ -17,8 +27,29 @@ class MainPage extends StatelessWidget {
     ];
     return Scaffold(
       appBar: AppBar(title: Text('Main Page')),
-      body: Center(child: Text('Welcome to the Main Page!')),
-      bottomNavigationBar: BottomNavigateBarCustom(items: items),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Text('This is the main page content', style: TextStyle(fontSize: 20)),
+          Center(child: Text('Welcome to the Main Page!')),
+        ],
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          items:
+              items
+                  .map(
+                    (item) => BottomNavigationBarItem(
+                      icon: item.icon,
+                      label: item.label,
+                      backgroundColor: Colors.blue,
+                    ),
+                  )
+                  .toList(),
+          currentIndex: controller.currentIndex.value,
+          onTap: controller.changeIndex,
+        ),
+      ),
     );
   }
 }
